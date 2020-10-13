e787c1f7eb03422a5af8af3035ff69902bde0b0e
Jack Ralph <jackralph@Jacks-MBP.local>
2020-10-13 20:06:58 +0100

    changed views to src

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"))
+    res.sendFile(path.join(__dirname, "/src/index.html"))
 })
 
 app.listen(3000);
