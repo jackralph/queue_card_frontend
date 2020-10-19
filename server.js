96a91e81a64fe34813d8d6d4feb4053231ac0b29
Jack Ralph <jackralph@Jacks-MBP.local>
2020-10-19 11:56:59 +0100

    router now routing correctly

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const app = express();
 
 app.use(express.static("public"))
 
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "/src/about.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, "signup.html"))
+})
+
+app.get("/terms_and_conditions", (req, res) => {
+    res.sendFile(path.join(__dirname, "terms_and_conditions.html"))
 })
 
 app.listen(3000);
