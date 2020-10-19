92908019b9262a4b2fe7542cfbdcbc308c4f8cab
Jack Ralph <jackralph@Jacks-MBP.local>
2020-10-19 12:00:39 +0100

    moved views into views folder

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "public/views/index.html"))
 })
 
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, "signup.html"))
+    res.sendFile(path.join(__dirname, "public/views/signup.html"))
 })
 
 app.get("/terms_and_conditions", (req, res) => {
-    res.sendFile(path.join(__dirname, "terms_and_conditions.html"))
+    res.sendFile(path.join(__dirname, "public/views/terms_and_conditions.html"))
 })
 
 app.listen(3000);
